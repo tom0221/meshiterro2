f0445aaece117d2760ecb21fca8c220283f68357
Tatsuro Muto <tomuto0221@gmail.com>
2020-06-25 00:54:14 +0900

    [finish]画像投稿機能

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '6ae7818b45b0de49208dd3bd24e86fe4a3ccec5e7a6ad98d88c96b1d232b6a58536aadb53fd3189963a3caa5d3778770017d53173c61dab067dc2721e4ce132e'
